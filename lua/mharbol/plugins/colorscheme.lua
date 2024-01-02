4489036c5b3e88a6f739fbc7f50c621ee928b323
Marcus Harbol <marcusharbol@gmail.com>
2024-01-02 14:45:28 -0800

    colorscheme to Kanagawa

--- lua/mharbol/plugins/colorscheme.lua
+++ lua/mharbol/plugins/colorscheme.lua
@@ -1,14 +1,30 @@
 return {
-    'folke/tokyonight.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000,
     config = function()
-        local tokyonight = require("tokyonight")
-        tokyonight.setup({
-            styles = {
-                comments = { italic = false },
-                keywords = { italic = false },
+        local kanagawa = require('kanagawa')
+        local colors = require("kanagawa.colors").setup({theme = "wave"})
+        local palette_colors = colors.palette
+        kanagawa.setup({
+            commentStyle = {
+                italic = false,
+            },
+            keywordStyle = {
+                italic = false,
+            },
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none",
+                        },
+                        vcs = {
+                            changed = palette_colors.waveBlue2,
+                        },
+                    },
+                },
             },
         })
-        vim.cmd.colorscheme("tokyonight-moon")
+        vim.cmd.colorscheme('kanagawa-wave')
     end,
 }
