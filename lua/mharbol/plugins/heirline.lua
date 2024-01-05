29e43de26ff4cae13410e0064e54ebc22b3dbc84
Marcus Harbol <mharbol@users.noreply.github.com>
2024-01-04 22:07:43 -0500

    lightbulb for help

--- lua/mharbol/plugins/heirline.lua
+++ lua/mharbol/plugins/heirline.lua
@@ -81,7 +81,7 @@ return {
                 error_icon = "  ",
                 warn_icon = "  ",
                 info_icon = "  ",
-                hint_icon = " 󰮦 ",
+                hint_icon = " 󱠃 ",
             },
             init = function(self)
                 self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
