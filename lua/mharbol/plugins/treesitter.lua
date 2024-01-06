d4a32d8285e98e48928aaa1d624669ccfebc0ca4
Marcus Harbol <mharbol@users.noreply.github.com>
2024-01-05 21:21:48 -0500

    python to treesitter

--- lua/mharbol/plugins/treesitter.lua
+++ lua/mharbol/plugins/treesitter.lua
@@ -21,6 +21,7 @@ return {
                 'latex',
                 'lua',
                 'markdown',
+                'python',
                 'rust',
                 'sql',
                 'toml',
