2130cf0ae68e4040504ba35d8b9ebeec1718d7cf
Marcus Harbol <mharbol@users.noreply.github.com>
2024-11-10 10:56:50 -0700

    update harpoon map

--- lua/mharbol/plugins/harpoon.lua
+++ lua/mharbol/plugins/harpoon.lua
@@ -12,6 +12,6 @@ return {
         vim.keymap.set('n', '<C-y>', function() ui.nav_file(1) end)
         vim.keymap.set('n', '<C-h>', function() ui.nav_file(2) end)
         vim.keymap.set('n', '<C-n>', function() ui.nav_file(3) end)
-        vim.keymap.set('n', '<C-t>', function() ui.nav_file(4) end)
+        vim.keymap.set('n', '<C-b>', function() ui.nav_file(4) end)
     end
 }
