38cb0772e25eccbc9dfe82931829d8caeb668002
Marcus Harbol <mharbol@users.noreply.github.com>
2026-03-03 22:15:19 -0700

    update mapings

--- lua/mharbol/plugins/completion.lua
+++ lua/mharbol/plugins/completion.lua
@@ -23,8 +23,8 @@ return {
             },
             mapping = cmp.mapping.preset.insert({
                 -- have to do it this way otherwise it messes with my tabs for some reason
-                ['<C-o>'] = cmp.mapping.select_prev_item(),
-                ['<C-i>'] = cmp.mapping.select_next_item(),
+                ['<C-k>'] = cmp.mapping.select_prev_item(),
+                ['<C-j>'] = cmp.mapping.select_next_item(),
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
